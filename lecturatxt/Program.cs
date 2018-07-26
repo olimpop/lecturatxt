ce218cc049367ca4474fa4adf6cce26dbdcc4eaa
olimpop <Jair Parra@PCJ>
2018-07-25 19:56:53 -0500

    Comentarios del documento.

--- lecturatxt/Program.cs
+++ lecturatxt/Program.cs
@@ -6,17 +6,20 @@ namespace lecturatxt
     {
         static void Main()
         {
+            //Inicializar la variable que realiza la lectura del archivo de texto
             var myreader = new System.IO.StreamReader("valores.txt");
+
             var line="";
             while (line!=null)
+                //Ciclo para recorrer el archivo hasta el final del documento
                 {
 	                line=myreader.ReadLine();
                     if (line!=null)
                     {
-                    Console.WriteLine("Informacion Linea leida: " + line.ToString());
-                    
+                    Console.WriteLine("Informacion Linea leida: " + line.ToString());                    
                 }
 	    }
+            //Cierre de la conexion
             myreader.Close();
             Console.ReadLine();
         }
