f2f82f56c3e6273e3df34ae24592a794f192cae7
olimpop <Jair Parra@PCJ>
2018-07-25 19:33:57 -0500

    Add project files.

---
+++ lecturatxt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lecturatxt
+{
+    class Program
+    {
+        static void Main()
+        {
+            var myreader = new System.IO.StreamReader("valores.txt");
+            var line="";
+            while (line!=null)
+                {
+	                line=myreader.ReadLine();
+                    if (line!=null)
+                    {
+                        Console.WriteLine(line)	 ;
+                    }
+	    }
+            myreader.Close();
+            Console.ReadLine();
+        }
+    }
+}
